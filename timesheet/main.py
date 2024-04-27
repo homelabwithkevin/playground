9aab0a0105f71e32e5b30b45640e389dcfd02904
Kevin <kevin@homelabwithkevin.com>
2024-04-26 22:27:25 -0400

    add generate random

--- timesheet/main.py
+++ timesheet/main.py
@@ -1,5 +1,6 @@
 from datetime import datetime
 import argparse
+import random
 
 def get_date(now):
     full_date = now.strftime("%Y-%m-%d %H:%M - %A")
@@ -7,6 +8,21 @@ def get_date(now):
     weekday = now.strftime('%A')
     week_number = str(now.isocalendar()[1])
     return full_date, date, weekday, week_number
+
+def generate_random_numbers(count=8):
+    numbers = []
+    for i in range(count):
+        numbers.append(random.randint(1, 9))
+
+    random_number = "".join(str(n) for n in numbers)
+    return random_number
+
+def generate_random_data(count=10):
+    names = []
+    for i in range(count):
+        number = generate_random_numbers()
+        names.append(f'PROJ-{number}')
+    print(names)
 
 now = datetime.now()
 full_date, date, weekday, week_number = get_date(now)
@@ -21,5 +37,6 @@ if args.action == 'test':
     print('test')
 elif args.action == 'generate_random_data':
     print('Generating random data...')
+    generate_random_data()
 else:
     print(f'Invalid action: {args.action}')
