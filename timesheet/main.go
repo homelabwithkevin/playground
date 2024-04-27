74efba50c910f2a6af37c9268cd4a5366d44f02c
Kevin <kevin@homelabwithkevin.com>
2024-04-27 08:27:14 -0400

    Add user input functionality to select a project in main.go

--- timesheet/main.go
+++ timesheet/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"strconv"
 )
 
 func main() {
@@ -10,5 +11,18 @@ func main() {
 	fmt.Printf("\n%s\n%s\n%s\n%d\n", full_date, date, weekday, week_number)
 
 	results := read_file(week_number)
-	fmt.Print(results)
+
+	var userInput string
+
+	for {
+		fmt.Print("\nSelect a project: ")
+		fmt.Scanln(&userInput)
+		userInput_int, err := strconv.Atoi(userInput)
+		if err != nil {
+			fmt.Printf("Invalid input: %s", userInput)
+		} else {
+			fmt.Printf(results[userInput_int])
+			break
+		}
+	}
 }
