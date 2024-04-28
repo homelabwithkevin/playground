56ceebbb9bd03d15ce546193eca137794338ce23
Kevin <kevin@homelabwithkevin.com>
2024-04-27 22:10:56 -0400

    install cloudflare adapter

--- photos/frontend/astro.config.mjs
+++ photos/frontend/astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from 'astro/config';
 
+import cloudflare from "@astrojs/cloudflare";
+
 // https://astro.build/config
 export default defineConfig({
-    output: 'server',
+  output: 'server',
+  adapter: cloudflare()
 });
