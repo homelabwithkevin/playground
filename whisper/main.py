1a179c45df35e33adcd10f737729427026e7af93
Kevin <kevin@homelabwithkevin.com>
2024-06-18 13:26:07 -0400

    use medium model

--- whisper/main.py
+++ whisper/main.py
@@ -8,15 +8,16 @@ import re
 # model = whisper.load_model("tiny")
 
 # Actual
-model = whisper.load_model("small")
+model = whisper.load_model("medium")
 
 result = model.transcribe("2024-06-18.mp3")
 
 text_result = result['text']
 
-print(text_result)
-
 text_result_split = re.split('break. |Break. |Break |Break.', text_result)
+
+with open('journal.txt', 'a') as f:
+    f.write('Whisper Transcription\n\n')
 
 for text in text_result_split:
     with open('journal.txt', 'a') as f:
