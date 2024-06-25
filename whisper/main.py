6c299e650d4e3ceefbb6f9f25f28866cf24f8aef
Kevin <kevin@homelabwithkevin.com>
2024-06-25 17:50:09 -0400

    support new parameters

--- whisper/main.py
+++ whisper/main.py
@@ -7,25 +7,22 @@ import re
 # Initial
 # model = whisper.load_model("tiny")
 
-# Actual
-model = whisper.load_model("medium")
-
-result = model.transcribe("2024-06-18.mp3")
-
-text_result = result['text']
-
-text_result_split = re.split('break. |Break. |Break |Break.', text_result)
-
-# Parameters
 mp3_file = '2024-06-25.mp3'
 journal_file = mp3_file.split('\\')[-1].split('.')[0] + '.md'
 
 enable_transcribe = True
 
 if enable_transcribe:
-    with open('journal.txt', 'a') as f:
+    model = whisper.load_model("medium")
+    result = model.transcribe(mp3_file)
+
+    text_result = result['text']
+
+    text_result_split = re.split('break. |Break. |Break |Break.|, break.| break ', text_result)
+
+    with open(journal_file, 'a') as f:
         f.write('Whisper Transcription\n\n')
 
     for text in text_result_split:
-        with open('journal.txt', 'a') as f:
+        with open(journal_file, 'a') as f:
             f.write(text.strip() + '\n\n')
