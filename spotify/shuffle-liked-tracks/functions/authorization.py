b04d0b309bc0ec53528474ec1985a1c5b05ed805
Kevin <kevin@homelabwithkevin.com>
2024-06-29 17:04:51 -0400

    initial authorization function

---
+++ spotify/shuffle-liked-tracks/functions/authorization.py
@@ -0,0 +1,2 @@
+def request_user_authorization(client_id, client_secret):
+    print(client_id, client_secret)
--- spotify/shuffle-liked-tracks/main.py
+++ spotify/shuffle-liked-tracks/main.py
@@ -1,12 +1,13 @@
 from dotenv import load_dotenv
 import os
 
+from functions import authorization
+
 # Load environment variables from .env file
 load_dotenv()
 
 # Accessing variables
 client_id = os.getenv('client_id')
-secret_id = os.getenv('secret_id')
+client_secret = os.getenv('secret_id')
 
-print(client_id)
-print(secret_id)
+authorization.request_user_authorization(client_id, client_secret)
