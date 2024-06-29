7c86e0621240833e4981f90528b03b7f72f4e4bd
Kevin <kevin@homelabwithkevin.com>
2024-06-29 18:35:43 -0400

    handle_liked_songs function

--- spotify/shuffle-liked-tracks/functions/utils.py
+++ spotify/shuffle-liked-tracks/functions/utils.py
@@ -5,3 +5,11 @@ def generate_random_string(length):
     letters = string.ascii_letters + string.digits
     random_string = ''.join(random.choice(letters) for i in range(length))
     return random_string
+
+def handle_liked_songs(songs):
+    list_liked_songs = []
+
+    for song in songs:
+        for item in song['items']:
+            list_liked_songs.append(item['track']['uri'])
+    return list_liked_songs
