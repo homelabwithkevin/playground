2b9b6b2df500de795d593e91ed8c0983ab54dcff
Kevin <kevin@homelabwithkevin.com>
2024-06-29 17:47:02 -0400

    create initial profile page

--- spotify/shuffle-liked-tracks/main.py
+++ spotify/shuffle-liked-tracks/main.py
@@ -1,6 +1,6 @@
 import os
 from dotenv import load_dotenv
-from flask import Flask, request, make_response, render_template
+from flask import Flask, request, make_response, render_template, redirect, url_for
 from functions import authorization
 
 # Load environment variables from .env file
@@ -25,15 +25,18 @@ def callback():
     access_token, refresh_token = authorization.request_access_token(client_id, client_secret, code)
 
     response = make_response(render_template('callback.html', access_token=access_token, refresh_token=refresh_token))
+
     response.set_cookie('access_token', access_token)
     response.set_cookie('refresh_token', refresh_token)
-    return response
+
+    return redirect(url_for('profile'))
 
 @app.route("/profile")
 def profile():
-    return f"""
-    <h1>Spotify Profile</h1>
-    <p>Profile: {authorization.get_profile()}</p>
-    """
+    access_token = request.cookies.get('access_token')
+    refresh_token = request.cookies.get('refresh_token')
+
+    response = make_response(render_template('callback.html', access_token=access_token, refresh_token=refresh_token))
+    return response
 
 app.run(port=8888)
