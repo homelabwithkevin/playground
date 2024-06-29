3fafcf21501b2c2d41ce0a261c9aac7fb6951d1a
Kevin <kevin@homelabwithkevin.com>
2024-06-29 18:35:30 -0400

    get liked songs function

--- spotify/shuffle-liked-tracks/functions/spotify.py
+++ spotify/shuffle-liked-tracks/functions/spotify.py
@@ -1,4 +1,5 @@
 import requests
+import json
 
 def get_user_profile(access_token):
     url = 'https://api.spotify.com/v1/me'
@@ -29,3 +30,38 @@ def create_playlist(access_token, name, user_id):
     response_json = response.json()
     playlist_id = response_json['id']
     return playlist_id
+
+def get_liked_songs(access_token):
+    all_tracks = []
+    url = 'https://api.spotify.com/v1/me/tracks'
+
+    headers = {
+        "Authorization": f"Bearer {access_token}",
+        "Content-Type": "application/json"
+    }
+
+    params = {'limit' : 50, 'offset': 0}
+
+    response = requests.get(url, headers=headers, params=params)
+
+    response_json = json.loads(response.content)
+
+    all_tracks.append(response_json)
+
+    if not response_json['next']:
+        return
+    else:
+        url = (response_json['next'])
+
+    while True:
+        break
+        response = requests.get(url, headers=headers, params=params)
+        response_json = json.loads(response.content)
+        all_tracks.append(response_json)
+
+        if not response_json['next']:
+            break
+        else:
+            url = (response_json['next'])
+
+    return all_tracks
