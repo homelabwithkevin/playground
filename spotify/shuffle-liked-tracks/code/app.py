ec9b0b74af92e9094791406fee8d791c2c5c3395
Kevin <kevin@homelabwithkevin.com>
2024-06-30 20:44:07 +0000

    add profile

--- spotify/shuffle-liked-tracks/code/app.py
+++ spotify/shuffle-liked-tracks/code/app.py
@@ -2,31 +2,60 @@ import os
 from functions import authorization, spotify, utils
 
 def lambda_handler(event, context):
+    print(event)
+
     client_id = os.getenv('CLIENT_ID')
     client_secret = os.getenv('CLIENT_SECRET')
     redirect_uri = os.getenv('REDIRECT_URI')
 
     print(client_id, client_secret, redirect_uri)
 
-    url = authorization.request_user_authorization(redirect_uri, client_id, client_secret)
+    path = event['path']
 
-    if (event['queryStringParameters']) and (event['queryStringParameters']['code']):
-        code = event['queryStringParameters']['code']
+    code = None
 
     res = {
-        'statusCode': 200,
         'headers': {
+            'statusCode': 200,
             'Content-Type': '*/*',
             'Set-Cookie': 'kevin=awesome'
         },
-        'body': f"""
-        <html>
-            <title>Spotify Login</title>
-            <h1>Spotify Login</h1>
-            <a href="{ url }" target="_blank">Login Here</a>
-            <p>Code: { code }</p>
-        </html>
-        """
     }
 
+    if path == '/callback':
+        if (event['queryStringParameters']) and (event['queryStringParameters']['code']):
+            code = event['queryStringParameters']['code']
+            res['headers']['Location'] = 'profile'
+            res['headers']['Set-Cookie'] = 'cats=awesome'
+            res['statusCode'] = 302
+            res['body'] = """
+            <html>
+                Code redirect
+            </html>
+            """
+    elif path == '/profile':
+        res['body'] = f"""
+        <html>
+            <title>Profile</title>
+            <h1>Profile</h1>
+            <p>Profile</p>
+        </html>
+        """
+    else:
+        url = authorization.request_user_authorization(redirect_uri, client_id, client_secret)
+
+        res = {
+            'statusCode': 200,
+            'headers': {
+                'Content-Type': '*/*',
+                'Set-Cookie': 'kevin=awesome'
+            },
+            'body': f"""
+            <html>
+                <title>Spotify Login</title>
+                <h1>Spotify Login</h1>
+                <a href="{ url }" target="_blank">Login Here</a>
+            </html>
+            """
+        }
     return res
