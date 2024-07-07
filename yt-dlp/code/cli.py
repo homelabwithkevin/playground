3488445a058fe3620b24e4dbee64fc2f4133e914
Kevin <kevin@homelabwithkevin.com>
2024-07-07 23:16:54 +0000

    create db function

--- yt-dlp/code/cli.py
+++ yt-dlp/code/cli.py
@@ -1,21 +1,12 @@
 import requests
 import boto3
 
-limit = 1 * 10
+from functions import utils, db
+
+limit = 1 * 2
 channel_id = "@MrBeast"
 url = f"http://127.0.0.1:5000/channel_id/{channel_id}?limit={limit}"
 table = "hlb-yt-dlp-TableCD117FA1-1B6DOH7B3U9CE"
-
-def put_item(item):
-    client = boto3.client('dynamodb', region_name='us-east-1')
-
-    response = client.put_item(
-        TableName=table,
-        Item=item
-    )
-
-    if not response['ResponseMetadata']['HTTPStatusCode'] == 200:
-        print(f'Failed to put_item')
 
 def request():
     response = requests.get(url)
@@ -29,7 +20,7 @@ def request():
                 'S': str(entry[value])
             }
 
-        put_item(item)
+        db.put_item(table, item)
         print(item)
 
 if __name__ == "__main__":
---
+++ yt-dlp/code/functions/db.py
@@ -0,0 +1,19 @@
+import boto3
+
+client = boto3.client('dynamodb', region_name='us-east-1')
+
+def put_item(table, item):
+    """
+        Put Item to DynamoDB Table
+
+        Args:
+            table (str): The name of the table.
+            item (dict): The item to put in the table.
+    """
+    response = client.put_item(
+        TableName=table,
+        Item=item
+    )
+
+    if not response['ResponseMetadata']['HTTPStatusCode'] == 200:
+        print(f'Failed to put_item')
