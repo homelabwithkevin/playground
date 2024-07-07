95664e273dce080daa4ede745c20ca6818b8baa8
Kevin <kevin@homelabwithkevin.com>
2024-07-07 00:35:26 +0000

    create time route

--- templ/main.go
+++ templ/main.go
@@ -2,10 +2,13 @@ package main
 
 import (
 	"net/http"
+	"time"
 	"github.com/a-h/templ"
 )
 
 func main() {
 	http.Handle("/", templ.Handler(hello()))
+	http.Handle("/time", templ.Handler(timeComponent(time.Now())))
+	http.Handle("/404", templ.Handler(notFoundComponent(), templ.WithStatus(http.StatusNotFound)))
 	http.ListenAndServe(":3000", nil)
 }
