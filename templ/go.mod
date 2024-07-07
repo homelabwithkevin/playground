148717e2bcd164f5f9f219f5501aeb19e94d12c2
Kevin <kevin@homelabwithkevin.com>
2024-07-07 00:57:21 +0000

    add session manager

--- templ/go.mod
+++ templ/go.mod
@@ -2,4 +2,7 @@ module main
 
 go 1.22.4
 
-require github.com/a-h/templ v0.2.747
+require (
+	github.com/a-h/templ v0.2.747
+	github.com/alexedwards/scs/v2 v2.8.0
+)
--- templ/main.go
+++ templ/main.go
@@ -5,15 +5,38 @@ import (
 	"time"
 	"github.com/a-h/templ"
 	"fmt"
+	"github.com/alexedwards/scs/v2"
 )
 
+var sessionManager *scs.SessionManager
+
+func putHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("putHandler")
+	sessionManager.Put(r.Context(), "meow", "kevin")
+}
+
+func getHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("GetHandler")
+	msg := sessionManager.GetString(r.Context(), "meow")
+	fmt.Println(msg)
+}
+
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-			fmt.Println(r.Context())
+	// Initialize the session.
+	sessionManager = scs.New()
+	sessionManager.Lifetime = 24 * time.Hour
+
+	fmt.Println(sessionManager)
+
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		 	putHandler(w, r)
 			hello().Render(r.Context(), w)
+		 	getHandler(w, r)
 	})
 
-	http.Handle("/time", templ.Handler(timeComponent(time.Now())))
-	http.Handle("/404", templ.Handler(notFoundComponent(), templ.WithStatus(http.StatusNotFound)))
-	http.ListenAndServe(":3000", nil)
+	mux.Handle("/time", templ.Handler(timeComponent(time.Now())))
+	mux.Handle("/404", templ.Handler(notFoundComponent(), templ.WithStatus(http.StatusNotFound)))
+	http.ListenAndServe(":3000", sessionManager.LoadAndSave(mux))
 }
