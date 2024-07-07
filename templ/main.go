0fe50f47aebc802f13d698612565102d652e8287
Kevin <kevin@homelabwithkevin.com>
2024-07-07 00:47:10 +0000

    use http responsewriter

--- templ/main.go
+++ templ/main.go
@@ -4,10 +4,15 @@ import (
 	"net/http"
 	"time"
 	"github.com/a-h/templ"
+	"fmt"
 )
 
 func main() {
-	http.Handle("/", templ.Handler(hello()))
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+			fmt.Println(r.Context())
+			hello().Render(r.Context(), w)
+	})
+
 	http.Handle("/time", templ.Handler(timeComponent(time.Now())))
 	http.Handle("/404", templ.Handler(notFoundComponent(), templ.WithStatus(http.StatusNotFound)))
 	http.ListenAndServe(":3000", nil)
