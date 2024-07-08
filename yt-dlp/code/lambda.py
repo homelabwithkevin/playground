8ac2b79959e4c7a77b4cfece483cca910cca8d8b
Kevin <kevin@homelabwithkevin.com>
2024-07-08 00:04:59 +0000

    add func function

--- yt-dlp/code/lambda.py
+++ yt-dlp/code/lambda.py
@@ -1,12 +1,15 @@
 import os
-from functions import db
+from functions import db, sqs
 
 table = "hlb-yt-dlp-TableCD117FA1-1B6DOH7B3U9CE"
+queue = os.getenv("QUEUE_URL")
 
 def handler(event, context):
     items = db.scan(table)
     for item in items:
         print(item)
+        sqs.publish(queue, item)
+        break
 
 if __name__ == "__main__":
     handler(None, None)
