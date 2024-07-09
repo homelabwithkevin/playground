e76b52bb3b78442a9722806ade872b380da21897
Kevin <kevin@homelabwithkevin.com>
2024-07-09 01:10:03 +0000

    add parsing of id

--- yt-dlp/code/func_queue.py
+++ yt-dlp/code/func_queue.py
@@ -1,3 +1,13 @@
+import json
+from functions import utils
 
 def handler(event, context):
     print(event)
+    for record in event['Records']:
+        message = json.loads(record['body'])
+        id = message['id']['S']
+        id = "erLbbextvlY"
+
+    URL = f'https://www.youtube.com/watch?v={id}'
+    result = utils.parse_video_information(URL=URL, VIDEO=True)
+    print(result)
