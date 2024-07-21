5bdc20e377ebe7cdce1343a671c5734599052379
Kevin <kevin@homelabwithkevin.com>
2024-07-21 18:22:34 +0000

    remove boiler

--- yt-dlp/code/test_api.py
+++ yt-dlp/code/test_api.py
@@ -1,18 +1,10 @@
 
 base_url = 'https://3mvh6tnzx3.execute-api.us-east-1.amazonaws.com/Prod?url'
-# https://3mvh6tnzx3.execute-api.us-east-1.amazonaws.com/Prod?url=https://youtu.be/4SNThp0YiU4
-# https://3mvh6tnzx3.execute-api.us-east-1.amazonaws.com/Prod?url=https://www.youtube.com/@MrBeast
-# https://3mvh6tnzx3.execute-api.us-east-1.amazonaws.com/Prod?url=https://www.youtube.com/@MrBeast/videos
-# https://3mvh6tnzx3.execute-api.us-east-1.amazonaws.com/Prod?url=https://youtu.be/4SNThp0YiU4
 
 import requests
 
 video_id = '4SNThp0YiU4'
 channel_id = '@MrBeast'
-
-def f():
-    return 3
-
 
 def test_share():
     test_url = f'{base_url}=https://youtu.be/{video_id}'
