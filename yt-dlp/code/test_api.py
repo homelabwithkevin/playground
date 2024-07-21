fea571aa1360168047c4693203a7af301468f429
Kevin <kevin@homelabwithkevin.com>
2024-07-21 18:20:00 +0000

    add test_video

--- yt-dlp/code/test_api.py
+++ yt-dlp/code/test_api.py
@@ -7,7 +7,6 @@ base_url = 'https://3mvh6tnzx3.execute-api.us-east-1.amazonaws.com/Prod?url'
 
 import requests
 
-
 video_id = '4SNThp0YiU4'
 channel_id = '@MrBeast'
 
@@ -17,6 +16,13 @@ def f():
 
 def test_share():
     test_url = f'{base_url}=https://youtu.be/{video_id}'
+    response = requests.get(test_url)
+    response_id = response.json()['id']
+    assert response.status_code == 200
+    assert response_id == video_id
+
+def test_video():
+    test_url = f'{base_url}=https://youtube.com/watch?={video_id}'
     response = requests.get(test_url)
     response_id = response.json()['id']
     assert response.status_code == 200
