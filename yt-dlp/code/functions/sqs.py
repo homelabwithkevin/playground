01192b474c5102175d70d5ef9eef2c017f1d5fd1
Kevin <kevin@homelabwithkevin.com>
2024-07-28 18:19:55 +0000

    update sqs

--- yt-dlp/code/functions/sqs.py
+++ yt-dlp/code/functions/sqs.py
@@ -1,10 +1,33 @@
 import boto3
 import json
 
-def publish(queue, message):
-    client = boto3.client('sqs')
 
+client = boto3.client('sqs', region_name='us-east-1')
+
+def publish(queue, message):
     client.send_message(
         QueueUrl=queue,
         MessageBody=json.dumps(message)
     )
+
+def delete_message(queue_url, receipt_handle):
+    client.delete_message(
+        QueueUrl=queue_url,
+        ReceiptHandle=receipt_handle
+    ) 
+
+def receive_message(queue_url):
+    response = client.receive_message(
+        QueueUrl=queue_url,
+        MessageSystemAttributeNames=["All"],
+        MaxNumberOfMessages=1,
+    )
+
+    if 'Messages' in response:
+        for message in response['Messages']:
+            receipt_handle = message['ReceiptHandle']
+            body = message['Body']
+            delete_message(queue_url, receipt_handle)
+            return body
+
+    return None
