9c610ad0ff54a22a8c0a568300fecdb30111ef86
Kevin <kevin@homelabwithkevin.com>
2024-07-28 20:15:35 +0000

    yt-dlp: feat: channels table

--- yt-dlp/code/api.py
+++ yt-dlp/code/api.py
@@ -5,6 +5,7 @@ import os
 from functions import utils, db, utils
 
 table_name = os.getenv('TABLE_NAME')
+table_channels = os.getenv('TABLE_CHANNELS')
 
 def lambda_handler(event, context):
     print(event)
@@ -35,6 +36,11 @@ def lambda_handler(event, context):
             db.put_item(
                 table_name,
                 utils.make_string_item(data=results['entries'][0], video=True)
+            )
+
+            db.put_item(
+                table_channels,
+                utils.make_string_item(data=results['info'], channel=True)
             )
 
     if not results:
--- yt-dlp/code/functions/utils.py
+++ yt-dlp/code/functions/utils.py
@@ -116,7 +116,7 @@ def parse_video_information(URL, VIDEO=False):
         logger.info(f'Complete - Parsing Video Information')
         return thumbnail, upload_date
 
-def make_string_item(data, video=False):
+def make_string_item(data, video=False, channel=False):
     """
         Creates an item for usage with DynamoDB from the key-value pairs of the given data.
 
@@ -129,12 +129,24 @@ def make_string_item(data, video=False):
     """
     item = {}
 
-    logger.info(f'Making String Items...video={video}')
+    logger.info(f'Making String Items...')
     for key, value in enumerate(data):
         if video:
             if value == 'id' or value == 'uploader_id' or value == 'duration' \
                 or value == 'title' or value == 'url' or value == 'like_count' or value == 'view_count' \
                 or value == 'comment_count' or value == 'channel_follower_count' or value == 'upload_date':
+
+                item[value] = {
+                    'S': str(data[value])
+                }
+
+        if channel:
+            if value == 'uploader_id' or value == 'title' or value == 'channel_follower_count':
+
+                if value == 'uploader_id':
+                    item['id'] = {
+                        'S': str(data[value])
+                    }
 
                 item[value] = {
                     'S': str(data[value])
