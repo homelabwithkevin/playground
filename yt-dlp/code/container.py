1a9ba9353317818d8b899d7190080e3111388f99
Kevin <kevin@homelabwithkevin.com>
2024-07-30 23:24:21 +0000

    add s3 function support to container

--- yt-dlp/code/container.py
+++ yt-dlp/code/container.py
@@ -7,9 +7,10 @@ import logging
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
-from functions import sqs, utils
+from functions import sqs, utils, s3
 
 queue_url = "https://sqs.us-east-1.amazonaws.com/654654599343/hlb-yt-dlp-QueueEC2-0H6PML5o97qd"
+bucket = 'hlb-yt-dlp'
 
 temp = True
 
@@ -23,6 +24,7 @@ url = json.loads(sqs.receive_message(queue_url=queue_url))
 if url:
     print(url)
     video_id = url.split('=')[1]
-    # utils.download(json.loads(url), '/media/')
+    utils.download(json.loads(url), '/media/')
+    s3.upload_file(f'/media/{video_id}/{video_id}.mp4', bucket, f'{video_id}/{video_id}.mp4')
 else:
     print('No messages.')
