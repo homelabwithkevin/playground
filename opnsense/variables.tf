12cb4d7af4c050d4e65319160c4602a8d72a3081
Kevin <kevin@homelabwithkevin.com>
2024-08-04 13:31:22 -0400

    add ovh server

--- opnsense/variables.tf
+++ opnsense/variables.tf
@@ -1,19 +1,20 @@
 variable "api_key" {}
 variable "api_secret" {}
+variable "ovh" {}
 variable "ovh_2024" {}
 variable "ovh_seedbox" {}
 
 variable "cloud_homelabwithkevin" {
-  type        = list(string)
-  default     = ["coder"]
+  type    = list(string)
+  default = ["coder"]
 }
 
 variable "homelabwithkevin" {
-  type        = list(string)
-  default     = [
+  type = list(string)
+  default = [
     "seedbox",
     "sonarr",
     "radarr",
     "rutorrent"
-    ]
+  ]
 }
