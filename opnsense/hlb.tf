af0bc5c496be748e76c49a52f8fe855b37d77e85
Kevin <kevin@homelabwithkevin.com>
2024-08-04 13:30:18 -0400

    add seedbox

--- opnsense/hlb.tf
+++ opnsense/hlb.tf
@@ -1,8 +1,8 @@
-module "homelabwithkevin" {
+module "seedbox" {
   for_each    = toset(var.homelabwithkevin)
   source      = "./modules/dns"
   description = "${each.key}-hlb"
   hostname    = each.key
-  domain      = "cloud.homelabwithkevin.com"
-  server      = var.ovh_2024
+  domain      = "homelabwithkevin.com"
+  server      = var.ovh_seedbox
 }
--- opnsense/variables.tf
+++ opnsense/variables.tf
@@ -1,9 +1,19 @@
 variable "api_key" {}
 variable "api_secret" {}
 variable "ovh_2024" {}
+variable "ovh_seedbox" {}
+
+variable "cloud_homelabwithkevin" {
+  type        = list(string)
+  default     = ["coder"]
+}
 
 variable "homelabwithkevin" {
   type        = list(string)
-  description = "list of names"
-  default     = ["coder"]
+  default     = [
+    "seedbox",
+    "sonarr",
+    "radarr",
+    "rutorrent"
+    ]
 }
