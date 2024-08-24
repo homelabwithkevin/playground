7accb31aff79badcd72bbd335d105b1722e1a245
Kevin <kevin@homelabwithkevin.com>
2024-08-24 09:19:17 -0400

    support method and object name in query

--- aws/pre-signed-url/code/app.py
+++ aws/pre-signed-url/code/app.py
@@ -1,10 +1,10 @@
 import boto3
 import os
 
-def generate_presigned_url(bucket_name, object_name, expiration=3600):
+def generate_presigned_url(method, bucket_name, object_name, expiration=3600):
     s3_client = boto3.client('s3')
     try:
-        response = s3_client.generate_presigned_url('get_object',
+        response = s3_client.generate_presigned_url(method,
                                                     Params={'Bucket': bucket_name,
                                                             'Key': object_name},
                                                     ExpiresIn=expiration)
@@ -15,10 +15,14 @@ def generate_presigned_url(bucket_name, object_name, expiration=3600):
     return response
 
 def lambda_handler(event, context):
-    bucket_name = os.getenv('BUCKET_NAME') or 'your-bucket-name'
-    object_name = 'kevin.txt'
+    bucket_name = os.getenv('BUCKET_NAME')
 
-    presigned_url = generate_presigned_url(bucket_name, object_name)
+    object_name = event['queryStringParameters']['object_name']
+    method = event['queryStringParameters']['method']
+
+    print(object_name, method)
+
+    presigned_url = generate_presigned_url(method, bucket_name, object_name)
 
     if presigned_url is not None:
         print(f"Generated pre-signed URL: {presigned_url}")
