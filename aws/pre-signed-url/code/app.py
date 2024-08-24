c8b9d3d59190b2d55b7faf6ee2a7bec54f3aadda
Kevin <kevin@homelabwithkevin.com>
2024-08-24 09:04:42 -0400

    generate presigned url

--- aws/pre-signed-url/code/app.py
+++ aws/pre-signed-url/code/app.py
@@ -1,5 +1,35 @@
+import boto3
+import os
+
+def generate_presigned_url(bucket_name, object_name, expiration=3600):
+    s3_client = boto3.client('s3')
+    try:
+        response = s3_client.generate_presigned_url('get_object',
+                                                    Params={'Bucket': bucket_name,
+                                                            'Key': object_name},
+                                                    ExpiresIn=expiration)
+    except Exception as e:
+        print(e)
+        return None
+
+    return response
+
 def lambda_handler(event, context):
+    bucket_name = os.getenv('BUCKET_NAME') or 'your-bucket-name'
+    object_name = 'kevin.txt'
+
+    presigned_url = generate_presigned_url(bucket_name, object_name)
+
+    if presigned_url is not None:
+        print(f"Generated pre-signed URL: {presigned_url}")
+    else:
+        print("Failed to generate pre-signed URL")
+        return {
+            'statusCode': 500,
+            'body': 'Failed to generate pre-signed URL'
+        }
+
     return {
         'statusCode': 200,
-        'body': 'Hello, World!'
+        'body': presigned_url
     }
