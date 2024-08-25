54b9c04d5fbc598bd6b288f839b81da1889867a5
Kevin <kevin@homelabwithkevin.com>
2024-08-25 17:43:24 -0400

    feat: aws - htmx dynamodb - form format and return message

--- aws/htmx-dynamodb/hello_world/app.py
+++ aws/htmx-dynamodb/hello_world/app.py
@@ -3,7 +3,7 @@ import os
 from datetime import datetime
 
 from functions import handler, utils
-
+from components import forms
 
 def lambda_handler(event, context):
     print(event)
@@ -14,14 +14,21 @@ def lambda_handler(event, context):
     if method == "POST":
         if event["body"]:
             message = event["body"].split("=")[1]
-            utils.put_item(message)
+            decoded_message = utils.url_decode(message)
+            utils.put_item(decoded_message)
+
+            new_message = "<br>"
+            for line in decoded_message.split("\n"):
+                new_message += f"{line}<br>"
+
             return {
                 "statusCode": 200,
                 "headers": {"Content-Type": "text/html"},
                 "body": f"""
                     <html>
                         <p id="content">
-                            Your Message: {message}
+                            Your Message:
+                            {new_message}
                         </p>
                     </html>
                 """,
@@ -31,26 +38,17 @@ def lambda_handler(event, context):
         return {
             "statusCode": 200,
             "headers": {"Content-Type": "text/html"},
-            "body": """
+            "body": f"""
                 <html>
                     <script src="https://cdn.tailwindcss.com"></script>
                     <script src="https://unpkg.com/htmx.org@2.0.2"></script>
                     <title>Homelab with Kevin</title>
-                    <div class="text-center mt-4">
-                        <div class="space-y-4">
+                    <div class="text-center mt-4 flex justify-center">
+                        <div class="space-y-4 min-w-[300px]">
                             <p>
                                 Leave a message!
                             </p>
-                            <p>
-                                <form hx-post="" hx-target="#content">
-                                    <p>
-                                        <input type="text" name="message" class="border-4 rounded-full border-black"/>
-                                    </p>
-                                    <p>
-                                        <button>Submit</button>
-                                    </p>
-                                </form>
-                            </p>
+                            {forms.form()}
                             <p id="content"></p>
                         </div>
                     </div>
--- aws/htmx-dynamodb/hello_world/functions/utils.py
+++ aws/htmx-dynamodb/hello_world/functions/utils.py
@@ -1,6 +1,7 @@
 import boto3
 import os
 from datetime import datetime
+from urllib.parse import unquote
 
 table_name = os.environ["TABLE_NAME"]
 
@@ -11,3 +12,6 @@ def put_item(message):
     dynamodb = boto3.resource("dynamodb")
     table = dynamodb.Table(table_name)
     table.put_item(Item={"id": today(), "message": message})
+
+def url_decode(message):
+    return unquote(message)
