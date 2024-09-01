a813f6ab23ffa046510268c725a38ee5f83183fc
Kevin <kevin@homelabwithkevin.com>
2024-09-01 17:11:42 -0400

    larger font for form

--- aws/cognito/hello_world/views/forms.py
+++ aws/cognito/hello_world/views/forms.py
@@ -4,7 +4,7 @@ def form_message(sub=None):
         <div class="col-span-full">
             <div class="mt-4 p-4">
                 <input id="user_id" value="{sub}" class="hidden">
-                <p class="mt-3 text-sm leading-6 text-gray-600">Write a message</p>
+                <p class="mt-3 mb-6 text-2xl leading-6 text-gray-600">Write a message</p>
                 <textarea id="message" name="message" rows="3" class="block w-full rounded-md border-0 p-4 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"></textarea>
             </div>
         </div>
