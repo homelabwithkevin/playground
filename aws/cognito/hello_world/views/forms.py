3c4560159d915962189f56dace782cf7dad6f2d0
Kevin <kevin@homelabwithkevin.com>
2024-09-01 19:48:24 -0400

    expand textarea

--- aws/cognito/hello_world/views/forms.py
+++ aws/cognito/hello_world/views/forms.py
@@ -5,7 +5,7 @@ def form_message(sub=None):
             <div class="mt-4 p-4">
                 <input id="user_id" value="{sub}" class="hidden">
                 <p class="mt-3 mb-6 text-2xl leading-6 text-gray-600">Write a message</p>
-                <textarea id="message" name="message" rows="3" class="block w-full rounded-md border-0 p-4 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"></textarea>
+                <textarea id="message" name="message" rows="3" class="min-w-[300px] block w-full rounded-md border-0 p-4 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"></textarea>
             </div>
         </div>
 
