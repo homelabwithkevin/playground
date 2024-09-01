09d0b0c78c0d64ebb29136fb62415c0a19087c8f
Kevin <kevin@homelabwithkevin.com>
2024-09-01 12:20:56 -0400

    create forms.py

---
+++ aws/cognito/hello_world/views/forms.py
@@ -0,0 +1,17 @@
+def form_message():
+    return """
+    <form hx-post="/Prod/post" hx-target="#result">
+        <div class="col-span-full">
+            <div class="mt-2">
+                <textarea id="message" name="message" rows="3" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"></textarea>
+            </div>
+            <p class="mt-3 text-sm leading-6 text-gray-600">Write a message</p>
+        </div>
+
+        <div class="mt-6 flex items-center justify-end gap-x-6">
+            <button type="button" class="text-sm font-semibold leading-6 text-gray-900">Cancel</button>
+            <button type="submit" class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Save</button>
+        </div>
+    </form>
+    <div id="result"></div>
+    """
