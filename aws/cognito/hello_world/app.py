338f80229c0875c423e0c681a374c74caebc5d45
Kevin <kevin@homelabwithkevin.com>
2024-09-01 17:35:19 -0400

    prep httpapi instead of restapi

--- aws/cognito/hello_world/app.py
+++ aws/cognito/hello_world/app.py
@@ -65,6 +65,7 @@ def lambda_handler(event, context):
         }
 
     return {
+        "isBase64Encoded": False,
         "statusCode": 200,
         "headers": {
             "Content-Type": "text/html",
