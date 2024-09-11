01349e5e6452e50d51b4a59dd92cf3021858e3fb
Kevin <kevin@homelabwithkevin.com>
2024-09-11 07:07:34 -0400

    add argparse and while loop

--- craps/main.py
+++ craps/main.py
@@ -1,4 +1,11 @@
 import random
+import argparse
+
+parser = argparse.ArgumentParser(description='Roll two dice')
+parser.add_argument('--count', '-c', type=int, default=1, help='Number of times to roll the dice')
+args = parser.parse_args()
+
+roll_count = args.count
 
 def roll_dice():
     return random.randint(1, 6), random.randint(1, 6)
@@ -6,9 +13,10 @@ def roll_dice():
 def calculate_sum_of_dice(dice_1, dice_2):
     return dice_1 + dice_2
 
-dice_1, dice_2 = roll_dice()
+total_rolls = 0
 
-sum_of_dice = calculate_sum_of_dice(dice_1, dice_2)
-
-print(sum_of_dice)
-
+while total_rolls < roll_count:
+    dice_1, dice_2 = roll_dice()
+    sum_of_dice = calculate_sum_of_dice(dice_1, dice_2)
+    print(sum_of_dice)
+    total_rolls += 1
