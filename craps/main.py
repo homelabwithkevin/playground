f1fb7038693d983be5ce5ea7bcf5eb2491794119
Kevin <kevin@homelabwithkevin.com>
2024-09-11 07:10:42 -0400

    save to file

--- craps/main.py
+++ craps/main.py
@@ -1,5 +1,7 @@
 import random
 import argparse
+import os
+from datetime import datetime
 
 parser = argparse.ArgumentParser(description='Roll two dice')
 parser.add_argument('--count', '-c', type=int, default=1, help='Number of times to roll the dice')
@@ -13,10 +15,21 @@ def roll_dice():
 def calculate_sum_of_dice(dice_1, dice_2):
     return dice_1 + dice_2
 
+def today():
+    return datetime.now().strftime('%Y-%m-%d-%H-%M-%S')
+
 total_rolls = 0
+
+output_file = f'{today()}.csv'
+with open(f'{output_file}', 'a') as f:
+    f.write(f'dice_1,dice_2,sum_of_dice\n')
 
 while total_rolls < roll_count:
     dice_1, dice_2 = roll_dice()
     sum_of_dice = calculate_sum_of_dice(dice_1, dice_2)
+
+    with open(f'{output_file}', 'a') as f:
+        f.write(f'{dice_1},{dice_2},{sum_of_dice}\n')
+
     print(sum_of_dice)
     total_rolls += 1
