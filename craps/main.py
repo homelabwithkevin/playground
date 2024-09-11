cb74b3973281f24b951acb15f7d16b5593de74f2
Kevin <kevin@homelabwithkevin.com>
2024-09-11 07:13:49 -0400

    add simple point calculation

--- craps/main.py
+++ craps/main.py
@@ -18,18 +18,28 @@ def calculate_sum_of_dice(dice_1, dice_2):
 def today():
     return datetime.now().strftime('%Y-%m-%d-%H-%M-%S')
 
+def calculate_point(sum_of_dice):
+    if sum_of_dice in [7]:
+        return 'loss'
+
+    return 'point'
+
 total_rolls = 0
 
 output_file = f'{today()}.csv'
 with open(f'{output_file}', 'a') as f:
-    f.write(f'dice_1,dice_2,sum_of_dice\n')
+    f.write(f'dice_1,dice_2,sum_of_dice,point\n')
 
 while total_rolls < roll_count:
     dice_1, dice_2 = roll_dice()
     sum_of_dice = calculate_sum_of_dice(dice_1, dice_2)
 
+    point = calculate_point(sum_of_dice)
+
     with open(f'{output_file}', 'a') as f:
-        f.write(f'{dice_1},{dice_2},{sum_of_dice}\n')
+        f.write(f'{dice_1},{dice_2},{sum_of_dice},{point}\n')
 
     print(sum_of_dice)
     total_rolls += 1
+
+print(f'Saved to: {output_file}')
