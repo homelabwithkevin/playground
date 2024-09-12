d96a9c9bda96e8db74888d22b7044412cb816b68
Kevin <kevin@homelabwithkevin.com>
2024-09-12 06:33:26 -0400

    missing variable for dice roll

--- craps/functions/dice.py
+++ craps/functions/dice.py
@@ -2,12 +2,13 @@ import random
 
 def roll(enable_magic=False):
     sum_of_dice = 0
+    result_magic = 0
 
     if enable_magic:
         magic_number = random.randint(1, 1000)
         result_magic = magic_number % 1
 
-    if result_magic == 0:
+    if result_magic == 1:
         dice_1, dice_2, sum_of_dice = magic_dice()
     else:
         dice_1 = random.randint(1, 6)
