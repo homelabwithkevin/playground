bd4ad9d174fd87fb8889c97d8e07091b134c75f2
Kevin <kevin@homelabwithkevin.com>
2024-09-15 12:33:14 -0400

    updates

--- aws/mailtrap/code/app.py
+++ aws/mailtrap/code/app.py
@@ -14,7 +14,8 @@ def randomword(length=10):
 
 def copy_object(key):
     extension = key.split('.')[-1]
-    new_key = f'cdn/{randomword()}.{extension}'
+    folder = key.split('/')[1]
+    new_key = f'cdn/{folder}/{randomword()}.{extension}'
     print(f'Copying {key} to {new_key}')
     client.copy_object(Bucket=bucket, CopySource=f'{bucket}/{key}', Key=new_key)
     print(f'Successful copy')
