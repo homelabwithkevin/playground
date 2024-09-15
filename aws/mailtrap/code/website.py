4e12d573fdd6ab1c60265821b070085dfe7f28f4
Kevin <kevin@homelabwithkevin.com>
2024-09-15 16:16:52 -0400

    add protected ip

--- aws/mailtrap/code/website.py
+++ aws/mailtrap/code/website.py
@@ -1,17 +1,44 @@
 import json
 import os
 
-from functions import form, handler
+from functions import form, handler, db
 
 cloudfront_url = os.getenv('CLOUDFRONT_URL')
+protected_ip = os.getenv('PROTECTED_IP') 
 
 def lambda_handler(event,context):
     request_context = event['requestContext']
     method = request_context['http']['method']
+    request_path = request_context['http']['path']
+
+    print(request_path)
 
     # User Information
     source_ip = request_context['http']['sourceIp']
 
+    if method == 'GET':
+        if request_path == '/emails':
+            if source_ip == protected_ip:
+                emails =  db.scan()
+                return {
+                    'statusCode': 200,
+                    'headers': {
+                        'Content-Type': 'text/html',
+                    },
+                    'body': emails
+                }
+            else:
+                return {
+                    'statusCode': 401,
+                    'headers': {
+                        'Content-Type': 'text/html',
+                    },
+                    'body': """
+                    <html>
+                        <div>Not authorized</div>
+                    </html>
+                    """
+                }
 
     if method == 'POST':
         return handler.post(event['body'], source_ip)
