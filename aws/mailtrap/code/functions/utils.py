16a76c4bacc6f42f4b179e10c98c9c305ea5d721
Kevin <kevin@homelabwithkevin.com>
2024-09-28 10:41:20 -0400

    add upload to utils

--- aws/mailtrap/code/functions/utils.py
+++ aws/mailtrap/code/functions/utils.py
@@ -6,3 +6,9 @@ def today():
 def today_newsletter():
     return datetime.now().strftime("%Y-%m-%d")
 
+def upload_file(bucket_name, file, content_type="image/jpeg"):
+    try:
+        client.upload_file(file, bucket_name, f'{newsletter}{file}', ExtraArgs={'ContentType': content_type})
+        print('File uploaded to S3')
+    except Exception as e:
+        print(f'Error uploading {file} to S3: {e}')
