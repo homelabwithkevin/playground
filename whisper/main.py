eb0fab8cb8d47d7d39c3a023fc0daa2027a1fd86
Kevin <116416787+homelabwithkevin@users.noreply.github.com>
2024-09-30 19:44:21 -0400

    feat: whisper - add for loop for mp3s (#45)

--- whisper/main.py
+++ whisper/main.py
@@ -1,25 +1,39 @@
 import whisper
 import re
+from datetime import datetime
+
+def get_date():
+    now = datetime.now().strftime("%Y-%m-%d-%H-%M-%S")
+    print(now)
+    return now
 
 # Available models:
 # tiny, base, small, medium, large
 
-mp3_file = '2024-06-25.mp3'
-journal_file = mp3_file.split('\\')[-1].split('.')[0] + '.md'
+mp3_files = [
+    'full_path.mp3'
+]
 
 enable_transcribe = True
+model_version = 'medium'
 
-if enable_transcribe:
-    model = whisper.load_model("medium")
-    result = model.transcribe(mp3_file)
+for mp3_file in mp3_files:
+    journal_file = mp3_file.split('\\')[-1].split('.')[0] + '.md'
 
-    text_result = result['text']
+    if enable_transcribe:
+        get_date()
+        model = whisper.load_model(model_version)
+        result = model.transcribe(mp3_file)
 
-    text_result_split = re.split('break. |Break. |Break |Break.|, break.| break ', text_result)
+        text_result = result['text']
 
-    with open(journal_file, 'a') as f:
-        f.write('Whisper Transcription\n\n')
+        text_result_split = re.split('break. |Break. |Break |Break.|, break.| break | brake. | brake', text_result)
 
-    for text in text_result_split:
         with open(journal_file, 'a') as f:
-            f.write(text.strip() + '\n\n')
+            f.write('Whisper Transcription\n\n')
+
+        for text in text_result_split:
+            with open(journal_file, 'a') as f:
+                f.write(text.strip() + '\n\n')
+
+        get_date()
