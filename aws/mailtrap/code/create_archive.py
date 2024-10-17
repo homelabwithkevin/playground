ab9553b8f9d9c0f0be984c6fcf136cb2de290d12
Kevin <kevin@homelabwithkevin.com>
2024-10-17 08:48:42 -0400

    use order in initial archive

--- aws/mailtrap/code/create_archive.py
+++ aws/mailtrap/code/create_archive.py
@@ -14,5 +14,23 @@ def initial_archive():
         '2024-10-13-newsletter',
     ]
 
-    for item in previous:
-        db.put_initial_archive_item(table, item)
+    for order, item in enumerate(previous):
+        db.put_initial_archive_item(table, order, item)
+
+archived_items = db.get_archive_items(table)
+
+html_code = "<html>"
+html_code += "<ul>"
+
+for item in archived_items:
+    print(item)
+    html_code += f"""
+    <li>{item}</li>
+    """
+
+html_code += "</html>"
+
+with open(f'archive.html', 'w') as f:
+    f.write(html_code)
+    print(f'Archive created')
+
