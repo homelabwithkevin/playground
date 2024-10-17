b40e576b79c4b59c4a9319eee878c14704ccae9c
Kevin <kevin@homelabwithkevin.com>
2024-10-17 08:32:27 -0400

    internal func

--- aws/mailtrap/code/create_archive.py
+++ aws/mailtrap/code/create_archive.py
@@ -5,13 +5,14 @@ from functions import db
 client = boto3.client('dynamodb')
 table = "hlb-mailtrap-archive-develop"
 
-previous = [
-    '2024-09-15-newsletter',
-    '2024-09-21-newsletter',
-    '2024-09-28-newsletter',
-    '2024-10-06-newsletter',
-    '2024-10-13-newsletter',
-]
+def initial_archive():
+    previous = [
+        '2024-09-15-newsletter',
+        '2024-09-21-newsletter',
+        '2024-09-28-newsletter',
+        '2024-10-06-newsletter',
+        '2024-10-13-newsletter',
+    ]
 
-for item in previous:
-    db.put_initial_archive_item(table, item)
+    for item in previous:
+        db.put_initial_archive_item(table, item)
