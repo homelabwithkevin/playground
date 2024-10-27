4cd132455d99055270bffc500e58e596076c8e8e
Kevin <kevin@homelabwithkevin.com>
2024-10-27 10:04:22 -0400

    parse date

--- pillow/main.py
+++ pillow/main.py
@@ -6,14 +6,24 @@ load_dotenv()
 
 base_path = os.getenv("BASE_PATH")
 
-def read_image(image):
+# https://stackoverflow.com/questions/23064549/get-date-and-time-when-photo-was-taken-from-exif-data-using-pil
+def read_image_date(image):
     img = PIL.Image.open(image)
     exif_data = img._getexif()
-    print(exif_data)
+    if not exif_data:
+        return None
 
+    return exif_data[36867]
+
+def parse_exif_date(exif_date):
+    year, month, day_hour, minute, second = exif_date.split(":")
+    print(year, month, day_hour, minute, second) 
+    print(exif_date)
 
 for root, dirs, files in os.walk(base_path):
     for file in files:
+        image_path = os.path.join(root, file)
         if file.endswith(".jpg"):
-            print(file)
+            image_date = read_image_date(image_path)
+            parse_exif_date(image_date) 
             break
