78cabe910726fa6e0dbb866fa850c5a356ef3ce2
Kevin <kevin@homelabwithkevin.com>
2024-11-12 16:22:36 -0500

    config rule

--- aws/config/main.py
+++ aws/config/main.py
@@ -18,4 +18,15 @@ def describe_rules():
     response = client.describe_config_rules()
     print(response)
 
-describe_rules()
+
+def put_organization_rule():
+    response = client.put_organization_config_rule(
+        OrganizationConfigRuleName='hlb-config-rule',
+        OrganizationManagedRuleMetadata={
+            'Description': 'Checks if the account password policy for AWS Identity and Access Management (IAM) users meets the specified requirements indicated in the parameters. The rule is NON_COMPLIANT if the account password policy does not meet the specified requirements.',
+            'RuleIdentifier': 'IAM_PASSWORD_POLICY',
+            'InputParameters': '{"RequireUppercaseCharacters":"true","RequireLowercaseCharacters":"true","RequireSymbols":"true","RequireNumbers":"true","MinimumPasswordLength":"14","PasswordReusePrevention":"24","MaxPasswordAge":"90"}',
+        },
+    )
+
+put_organization_rule()
