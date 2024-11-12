9181588e684e4e1587ef1437ceda17d49c8ebbfe
Kevin <kevin@homelabwithkevin.com>
2024-11-12 16:15:32 -0500

    add describe rules

--- aws/config/main.py
+++ aws/config/main.py
@@ -1,10 +1,11 @@
 # https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/config.html
+# https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/config/client/put_organization_config_rule.html
 
 import boto3
 
 client = boto3.client('config')
 
-def describe_rules():
+def describe_organization_rules():
     response = client.describe_organization_config_rules()
     rules = response['OrganizationConfigRules']
 
@@ -12,3 +13,9 @@ def describe_rules():
         return rules
     else:
         return None
+
+def describe_rules():
+    response = client.describe_config_rules()
+    print(response)
+
+describe_rules()
