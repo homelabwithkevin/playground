e8554ba956c88d74062284f121c60d485b7c4afa
Kevin <kevin@homelabwithkevin.com>
2024-11-24 19:35:20 -0500

    add sns topic

--- motioneye/code/app.py
+++ motioneye/code/app.py
@@ -1,7 +1,26 @@
 import json
+import os
+import boto3
+
+topic = os.environ['TOPIC']
+
+def publish(topic, message=None):
+    print(f'Publishing to {topic}')
+
+    client = boto3.client('sns')
+    topic_message = 'Motion detected'
+
+    if message:
+        topic_message = message
+
+    response = client.publish(
+        TopicArn = topic,
+        Message = topic_message
+    )
 
 def lambda_handler(event, context):
-    print(event)
+    publish(topic, message)
+
     return {
         "statusCode": 200,
         "body": json.dumps({
