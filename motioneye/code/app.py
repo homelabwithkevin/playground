311525c8c07404e25821e050f4770d87af976667
Kevin <kevin@homelabwithkevin.com>
2024-11-24 19:39:51 -0500

    add topic permission and publish func

--- motioneye/code/app.py
+++ motioneye/code/app.py
@@ -1,25 +1,31 @@
 import json
 import os
 import boto3
+from datetime import datetime
 
 topic = os.environ['TOPIC']
 
-def publish(topic, message=None):
+def today():
+    return datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+
+def publish(topic, message=None, subject=None):
     print(f'Publishing to {topic}')
 
     client = boto3.client('sns')
     topic_message = 'Motion detected'
+    topic_subject = f'MotionEye - {today()}'
 
     if message:
         topic_message = message
 
     response = client.publish(
         TopicArn = topic,
-        Message = topic_message
+        Message = topic_message,
+        Subject = topic_subject
     )
 
 def lambda_handler(event, context):
-    publish(topic, message)
+    publish(topic)
 
     return {
         "statusCode": 200,
