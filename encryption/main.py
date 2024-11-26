07ac7ed8f89707bd76061cdfbe6a88662e2229d3
Kevin <kevin@homelabwithkevin.com>
2024-11-25 20:08:38 -0500

    basic encpryiton

--- encryption/main.py
+++ encryption/main.py
@@ -1,10 +1,27 @@
-from cryptography.fernet import Fernet
+import base64
+import os
 
-key = Fernet.generate_key()
-print(key)
+from cryptography.fernet import Fernet
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
+
+password = b'password'
+
+salt = os.urandom(16)
+
+kdf = PBKDF2HMAC(
+    algorithm=hashes.SHA256(),
+    length=32,
+    salt=salt,
+    iterations=480000,
+)
+
+key = base64.urlsafe_b64encode(kdf.derive(password))
 
 f = Fernet(key)
 
-token = f.encrypt(b'Testing')
+token = f.encrypt(b'Secret')
 
 print(token)
+
+print(f.decrypt(token))
