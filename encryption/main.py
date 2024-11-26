b99a9ae689a8eaa45895fe65a7e676f8205e47e4
Kevin <kevin@homelabwithkevin.com>
2024-11-25 20:14:02 -0500

    use functions

--- encryption/main.py
+++ encryption/main.py
@@ -7,23 +7,23 @@ from cryptography.fernet import Fernet
 from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
 
-password = b'password'
+def generate_salt(lengt=16):
+    salt = os.urandom(16)
+    return salt
 
-salt = os.urandom(16)
+def generate_key(salt, password):
+    kdf = PBKDF2HMAC(
+        algorithm=hashes.SHA256(),
+        length=32,
+        salt=salt,
+        iterations=480000,
+    )
 
-kdf = PBKDF2HMAC(
-    algorithm=hashes.SHA256(),
-    length=32,
-    salt=salt,
-    iterations=480000,
-)
+    key = base64.urlsafe_b64encode(kdf.derive(password))
 
-key = base64.urlsafe_b64encode(kdf.derive(password))
+    return key
 
-f = Fernet(key)
-
-token = f.encrypt(b'Secret')
-
-print(token)
-
-print(f.decrypt(token))
+def encrypt(key, message):
+    f = Fernet(key)
+    token = f.encrypt(message)
+    return token
