e802867490e7ca170d981cb17ef49743a1b4b83d
Kevin <kevin@homelabwithkevin.com>
2024-11-25 20:53:32 -0500

    accept user inputs

--- encryption/main.py
+++ encryption/main.py
@@ -5,6 +5,7 @@ import os
 import argparse
 import random
 import binascii
+import codecs
 
 parser = argparse.ArgumentParser(description='Encrypt a message with a password')
 parser.add_argument('option', type=str, help='Encrypt/decrypt')
@@ -15,11 +16,9 @@ from cryptography.fernet import Fernet
 from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
 
-salt = b'temporary'
-
 def generate_salt(length=16):
     random = os.urandom(length)
-    salt = binascii.hexlify(random)
+    salt = binascii.hexlify(random).decode()
     print(f'Salt: {salt}')
     return salt
 
@@ -27,7 +26,7 @@ def generate_key(salt, password):
     kdf = PBKDF2HMAC(
         algorithm=hashes.SHA256(),
         length=32,
-        salt=salt,
+        salt=salt.encode(),
         iterations=480000,
     )
     key = base64.urlsafe_b64encode(kdf.derive(password))
@@ -35,31 +34,42 @@ def generate_key(salt, password):
 
 def encrypt(key, message):
     f = Fernet(key)
-    encrypted_message = f.encrypt(message)
+    encrypted_message = f.encrypt(message).decode()
     print(f'Encypted Message: {encrypted_message}')
     return encrypted_message
 
 def decrypt(key, message):
     f = Fernet(key)
     decrypted_message = f.decrypt(message)
-    print(f'Decrypted Message: {decrypted_message}')
-    return decrypted_message
+    # https://stackoverflow.com/questions/4020539/process-escape-sequences-in-a-string-in-python
+    codec_message = codecs.escape_decode(decrypted_message, "utf-8")[0].decode("utf-8")
+    print(f'\nDecrypted Message: \n{codec_message}')
+    return codec_message
 
 if args.option and args.password:
     option = args.option
     password = (args.password).encode()
 
     if option == 'encrypt':
-        # salt = generate_salt()
+        salt = generate_salt()
         key = generate_key(salt, password)
-        message = input('Enter the message to encrypt: ')
+        lines = []
+        while True:
+            line = input()
+            if line:
+                lines.append(line)
+            else:
+                break
+
+        message = '\n'.join(lines)
+
         encrypt(key, message.encode())
 
     elif option == 'decrypt':
-        # salt = b'2a56e4c43535a3e7a3ac9692e5094aad'
+        salt = input('Paste your salt: \n')
+        message = input('Paste your encrypted message: \n')
         key = generate_key(salt, password)
-        message = b'gAAAAABnRSawMpsKjV4aclf_NSsX37bx42-AhuZkfwImOdpxsF_T0Jkw1vJ2K3B4cJA42zO4Yyy15Zu8PXw7_OZSPhebl1yHU3itJTjViXPZMTVUO9oTv0w='
-        decrypt(key, message)
+        decrypt(key, message.encode())
     else:
         print(f'Invalid option')
 
