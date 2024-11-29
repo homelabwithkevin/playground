fecd5653512e66f881dde35afb640410f9a5f84b
Kevin <kevin@homelabwithkevin.com>
2024-11-29 11:28:21 -0500

    basic read csv func

--- aws/mailtrap/code/migrate_csv_table_posts.py
+++ aws/mailtrap/code/migrate_csv_table_posts.py
@@ -1,10 +1,15 @@
 import boto3
 import os
 import pandas as pd
+import json
 
 client = boto3.client('dynamodb')
 table = 'hlb-mailtrap-posts-develop'
 
-df = pd.read_csv('2024-09-18.csv')
+file = '2024-09-18.csv'
 
-print(df)
+def read_csv(file):
+    data = pd.read_csv(file, encoding='utf-8')
+    data_json = json.loads(data.to_json(orient='records'))
+    file_json = { file: data_json }
+    return file_json
