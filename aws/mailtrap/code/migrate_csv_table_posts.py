4d649ea575cb76f0832e5b3319d13f52e0c0ca1a
Kevin <kevin@homelabwithkevin.com>
2024-11-29 11:45:09 -0500

    iterate through

--- aws/mailtrap/code/migrate_csv_table_posts.py
+++ aws/mailtrap/code/migrate_csv_table_posts.py
@@ -6,13 +6,10 @@ import json
 client = boto3.client('dynamodb')
 table = 'hlb-mailtrap-posts-develop'
 
-file = '2024-09-18.csv'
-
 def read_csv(file):
     data = pd.read_csv(file, encoding='utf-8')
     data_json = json.loads(data.to_json(orient='records'))
-    file_json = { file: data_json }
-    return file_json
+    return file, data_json
 
 def list_files():
     files = []
@@ -22,3 +19,9 @@ def list_files():
     return files
 
 files = list_files()
+list_of_data = {}
+
+for file in files:
+    file, data = read_csv(file)
+    list_of_data[file] = data
+    break
