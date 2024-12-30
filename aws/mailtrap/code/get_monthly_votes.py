3b7d0ec83bb587791ab06e78c9a14c36cd3084e0
Kevin <kevin@homelabwithkevin.com>
2024-12-30 09:58:57 -0500

    iterate through data

--- aws/mailtrap/code/get_monthly_votes.py
+++ aws/mailtrap/code/get_monthly_votes.py
@@ -1,4 +1,5 @@
 import os
+import pandas as pd
 
 from dotenv import load_dotenv
 from functions import db
@@ -14,8 +15,28 @@ def get_votes(newsletter='2024-12-28'):
 
 archived_items = db.get_archive_items(table_archive)
 
+totals = []
+
+# Iterate through Archived Items
+# For Each Archived Item, Get Votes for all pictures
+# Return Pandas Dataframe with all the data
+
 for archived_item in archived_items:
     newsletter = archived_item.split('-newsletter')[0]
     votes = get_votes(newsletter)
-    print(newsletter, votes)
+    _totals = {}
+    for key, file in enumerate(votes):
+        _totals = {
+            'newsletter': newsletter,
+            'rank': key,
+            'file': file,
+            'file': file,
+            'votes': votes[file]
+        }
+
+        totals.append(_totals)
     break
+
+df = pd.DataFrame(totals)
+print(df)
+# df.to_csv('totals.csv', index=False)
