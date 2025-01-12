7c132aee0033f8c77b8a99de0a73b8e0d39dd9fa
Kevin <kevin@homelabwithkevin.com>
2025-01-12 12:24:40 -0500

    add status haproxy

--- opnsense/apis/main.py
+++ opnsense/apis/main.py
@@ -1,11 +1,22 @@
 import os
+import requests
+import json
+from requests.auth import HTTPBasicAuth
 from dotenv import load_dotenv
 load_dotenv()
 
 key = os.getenv("key")
 secret = os.getenv("secret")
+base_url = os.getenv("base_url")
 
 # Base URL
 # https://opnsense.local/api/<module>/<controller>/<command>/[<param1>/[<param2>/...]]
 
-print(key, secret)
+haproxy_url = f'{base_url}/haproxy/service/status'
+
+def status():
+    response = requests.get(haproxy_url, auth=HTTPBasicAuth(key, secret), verify=False)
+    return json.loads(response.content)
+
+
+print(status())
