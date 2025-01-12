370e9709507b6b52a3b26de817cbcc9eb3ce7210
Kevin <kevin@homelabwithkevin.com>
2025-01-12 12:46:41 -0500

    add add server

--- opnsense/apis/main.py
+++ opnsense/apis/main.py
@@ -8,6 +8,7 @@ load_dotenv()
 key = os.getenv("key")
 secret = os.getenv("secret")
 base_url = os.getenv("base_url")
+headers = {'Content-Type':'application/json'}
 
 # Base URL
 # https://opnsense.local/api/<module>/<controller>/<command>/[<param1>/[<param2>/...]]
@@ -33,3 +34,34 @@ def backend(action):
         response = requests.get(url, auth=HTTPBasicAuth(key, secret), verify=False)
         response_json = json.loads(response.content)
         return response_json['rows']
+
+def server(action='search', name=None, description=None, address=None, port=None):
+    if action == 'search':
+        url = f'{settings_url}/searchServers'
+        response = requests.get(url, auth=HTTPBasicAuth(key, secret), verify=False)
+        response_json = json.loads(response.content)
+        return response_json['rows']
+
+    if action == 'add':
+        url = f'{settings_url}/addServer'
+        content = {
+            "server":{
+                "enabled": "1",
+                "name": name,
+                "description": description,
+                "type": "static",
+                "address": address,
+                "port": port,
+                "mode": "active",
+                "multiplexer_protocol": "unspecified",
+                "ssl": "0",
+                "sslSNI": "",
+                "sslVerify": "0",
+            }
+        }
+        response = requests.post(url, auth=HTTPBasicAuth(key, secret), verify=False,headers=headers, data=json.dumps(content))
+        response_json = json.loads(response.content)
+        print(response_json)
+        return response_json
+
+server('add', name='kevin', description='kevin',address='127.0.0.1', port='69')
