610a7493f0a9d5a0d6f93adc411ec86410d8cc2b
Kevin <kevin@homelabwithkevin.com>
2025-01-20 08:55:39 -0500

    add get path func

--- albums/main.py
+++ albums/main.py
@@ -17,16 +17,21 @@ def walk_directory(path):
         for file in files:
             yield os.path.join(root, file)
 
-for file in walk_directory(base_path):
-    if file.lower().endswith(('.jpg', '.jpeg', '.png')):
-        with Image.open(file) as image:
-            exif_data = image._getexif()
-            if exif_data:
-                for tag, value in exif_data.items():
-                    tag_name = TAGS.get(tag, tag)
-                    if tag_name == 'DateTimeOriginal':
-                        # 2007:02:18 14:07:30"
-                        year, month, day = (value.split(" "))[0].split(":")
-                        if year == search_year and month == search_month:
-                            if day in search_days:
-                                print(file)
+def write_to_file(content, file):
+    with open(file, 'a') as f:
+        f.write(content + '\n')
+
+def get_photos(path):
+    for file in walk_directory(path):
+        if file.lower().endswith(('.jpg', '.jpeg', '.png')):
+            with Image.open(file) as image:
+                exif_data = image._getexif()
+                if exif_data:
+                    for tag, value in exif_data.items():
+                        tag_name = TAGS.get(tag, tag)
+                        if tag_name == 'DateTimeOriginal':
+                            # 2007:02:18 14:07:30"
+                            year, month, day = (value.split(" "))[0].split(":")
+                            if year == search_year and month == search_month:
+                                if day in search_days:
+                                    write_to_file(file, 'photos.csv')
