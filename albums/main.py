50d5785211bb74766f5410803d90f20cacceccc0
Kevin <kevin@homelabwithkevin.com>
2025-01-20 09:23:48 -0500

    add create html func

--- albums/main.py
+++ albums/main.py
@@ -36,25 +36,26 @@ def get_photos(path):
                                 if day in search_days:
                                     write_to_file(file, 'photos.csv')
 
-with open('photos.csv', 'r') as f:
-    lines = f.readlines()
-    content = ""
-    content += '<script src="https://cdn.tailwindcss.com"></script>'
-    content += '<div class="flex flex-wrap justify-center">'
-    content += '<div class="grid grid-cols-6 gap-4">'
-    x = 0
-    for line in lines:
-        if x == 0:
-            x += 1
-            pass
+def create_html(source_file='photos.csv', output_file='index.html'):
+    with open(source_file, 'r') as f:
+        lines = f.readlines()
+        content = ""
+        content += '<script src="https://cdn.tailwindcss.com"></script>'
+        content += '<div class="flex flex-wrap justify-center">'
+        content += '<div class="grid grid-cols-6 gap-4">'
+        x = 0
+        for line in lines:
+            if x == 0:
+                x += 1
+                pass
 
-        tag, file = line.split(",")
-        content += "<a target='_blank' href='file://" + file + "'>"
-        content += "<img width='500px' height='700px' src='file://" + file + "' />\n"
-        content += "</a>"
+            tag, file = line.split(",")
+            content += "<a target='_blank' href='file://" + file + "'>"
+            content += "<img width='500px' height='700px' src='file://" + file + "' />\n"
+            content += "</a>"
 
-    content += '</div>'
-    content += '</div>'
+        content += '</div>'
+        content += '</div>'
 
-with open('photos.html', 'w') as f:
-    f.write(content)
+    with open(output_file, 'w') as f:
+        f.write(content)
