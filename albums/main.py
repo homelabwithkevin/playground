15b4759294bb956b90bc0d4e32e10ddb62384e06
Kevin <kevin@homelabwithkevin.com>
2025-01-20 08:48:05 -0500

    walk directory

--- albums/main.py
+++ albums/main.py
@@ -4,4 +4,11 @@ from dotenv import load_dotenv
 load_dotenv()
 
 base_path = os.getenv("BASE_PATH")
-print(base_path)
+
+def walk_directory(path):
+    for root, dirs, files in os.walk(path):
+        for file in files:
+            yield os.path.join(root, file)
+
+for file in walk_directory(base_path):
+    print(file)
