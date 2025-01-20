8bbaf2bf5d62ea3b94cbc16eb4290f091d6a0d70
Kevin <kevin@homelabwithkevin.com>
2025-01-20 08:53:24 -0500

    search yeart month and days

--- albums/main.py
+++ albums/main.py
@@ -1,9 +1,16 @@
 import os
+
+from PIL import Image
+from PIL.ExifTags import TAGS
+
 from dotenv import load_dotenv
 
 load_dotenv()
 
 base_path = os.getenv("BASE_PATH")
+search_year = '2025'
+search_month = '01'
+search_days = ['17', '18', '19']
 
 def walk_directory(path):
     for root, dirs, files in os.walk(path):
@@ -11,4 +18,15 @@ def walk_directory(path):
             yield os.path.join(root, file)
 
 for file in walk_directory(base_path):
-    print(file)
+    if file.lower().endswith(('.jpg', '.jpeg', '.png')):
+        with Image.open(file) as image:
+            exif_data = image._getexif()
+            if exif_data:
+                for tag, value in exif_data.items():
+                    tag_name = TAGS.get(tag, tag)
+                    if tag_name == 'DateTimeOriginal':
+                        # 2007:02:18 14:07:30"
+                        year, month, day = (value.split(" "))[0].split(":")
+                        if year == search_year and month == search_month:
+                            if day in search_days:
+                                print(file)
