13e1240c9452f7c86291351bbc62901c8981bf1d
Kevin <kevin@homelabwithkevin.com>
2025-01-25 18:19:42 -0500

    reference existing implementation

--- encryption/main.go
+++ encryption/main.go
@@ -1,13 +1,65 @@
 package main
 
+// https://gist.github.com/tscholl2/dc7dc15dc132ea70a98e8542fefffa28
+
 import (
+	"bufio"
+	"crypto/aes"
+	"crypto/cipher"
+	"crypto/rand"
 	"crypto/sha256"
+	"encoding/hex"
 	"fmt"
+	"os"
+	"strings"
+
 	"golang.org/x/crypto/pbkdf2"
 )
 
+func deriveKey(passphrase string, salt []byte) ([]byte, []byte) {
+	if salt == nil {
+		salt = make([]byte, 8)
+		// http://www.ietf.org/rfc/rfc2898.txt
+		// Salt.
+		rand.Read(salt)
+	}
+	return pbkdf2.Key([]byte(passphrase), salt, 1000, 32, sha256.New), salt
+}
+
+func encrypt(passphrase, plaintext string) string {
+	key, salt := deriveKey(passphrase, nil)
+	iv := make([]byte, 12)
+	// http://nvlpubs.nist.gov/nistpubs/Legacy/SP/nistspecialpublication800-38d.pdf
+	// Section 8.2
+	rand.Read(iv)
+	b, _ := aes.NewCipher(key)
+	aesgcm, _ := cipher.NewGCM(b)
+	data := aesgcm.Seal(nil, iv, []byte(plaintext), nil)
+	return hex.EncodeToString(salt) + "-" + hex.EncodeToString(iv) + "-" + hex.EncodeToString(data)
+}
+
+func decrypt(passphrase, ciphertext string) string {
+	arr := strings.Split(ciphertext, "-")
+	salt, _ := hex.DecodeString(arr[0])
+	iv, _ := hex.DecodeString(arr[1])
+	data, _ := hex.DecodeString(arr[2])
+	key, _ := deriveKey(passphrase, salt)
+	b, _ := aes.NewCipher(key)
+	aesgcm, _ := cipher.NewGCM(b)
+	data, _ = aesgcm.Open(nil, iv, data, nil)
+	return string(data)
+}
+
 func main() {
-	salt := make([]byte, 16)
-	dk := pbkdf2.Key([]byte("password"), salt, 4096, 32, sha256.New)
-	fmt.Printf("%x\n", dk)
+	// passphrase, text
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Printf("Enter passphrase: ")
+	passphrase, _ := reader.ReadString('\n')
+
+	fmt.Printf("Enter text: ")
+	text, _ := reader.ReadString('\n')
+
+	c := encrypt(passphrase, text)
+	fmt.Println(c)
+	fmt.Println(decrypt(passphrase, "4d67d40da48f8037-c4be29fcb37c93e19a272bf5-6bd79e7a7325f6275dca7d0680190b22e5c531456a06"))
 }
