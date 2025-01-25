b6a86c0325501b0c56a9bbd8502e1a48821a24bf
Kevin <kevin@homelabwithkevin.com>
2025-01-25 17:59:53 -0500

    initial

--- encryption/main.go
+++ encryption/main.go
@@ -1,7 +1,13 @@
 package main
 
-import "fmt"
+import (
+	"crypto/sha256"
+	"fmt"
+	"golang.org/x/crypto/pbkdf2"
+)
 
 func main() {
-	fmt.Println("Hello World")
+	salt := make([]byte, 16)
+	dk := pbkdf2.Key([]byte("password"), salt, 4096, 32, sha256.New)
+	fmt.Printf("%x\n", dk)
 }
