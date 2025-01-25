5bb16eaffb20648b59536513f80e65598e7f5a03
Kevin <116416787+homelabwithkevin@users.noreply.github.com>
2025-01-25 18:34:19 -0500

    feat: basic encryption with golang (#95)

---
+++ encryption/go.mod
@@ -0,0 +1,5 @@
+module encryption
+
+go 1.23.2
+
+require golang.org/x/crypto v0.32.0
---
+++ encryption/main.go
@@ -0,0 +1,69 @@
+package main
+
+// https://gist.github.com/tscholl2/dc7dc15dc132ea70a98e8542fefffa28
+
+import (
+	"bufio"
+	"crypto/aes"
+	"crypto/cipher"
+	"crypto/rand"
+	"crypto/sha256"
+	"encoding/hex"
+	"fmt"
+	"os"
+	"strings"
+
+	"golang.org/x/crypto/pbkdf2"
+)
+
+func deriveKey(passphrase string, salt []byte) ([]byte, []byte) {
+	if salt == nil {
+		salt = make([]byte, 8)
+		// http://www.ietf.org/rfc/rfc2898.txt
+		// Salt.
+		rand.Read(salt)
+	}
+	return pbkdf2.Key([]byte(passphrase), salt, 1000, 32, sha256.New), salt
+}
+
+func encrypt(passphrase, plaintext string) string {
+	key, salt := deriveKey(passphrase, nil)
+	iv := make([]byte, 12)
+	// http://nvlpubs.nist.gov/nistpubs/Legacy/SP/nistspecialpublication800-38d.pdf
+	// Section 8.2
+	rand.Read(iv)
+	b, _ := aes.NewCipher(key)
+	aesgcm, _ := cipher.NewGCM(b)
+	data := aesgcm.Seal(nil, iv, []byte(plaintext), nil)
+	return hex.EncodeToString(salt) + "-" + hex.EncodeToString(iv) + "-" + hex.EncodeToString(data)
+}
+
+func decrypt(passphrase, ciphertext string) string {
+	arr := strings.Split(ciphertext, "-")
+	salt, _ := hex.DecodeString(arr[0])
+	iv, _ := hex.DecodeString(arr[1])
+	data, _ := hex.DecodeString(arr[2])
+	key, _ := deriveKey(passphrase, salt)
+	b, _ := aes.NewCipher(key)
+	aesgcm, _ := cipher.NewGCM(b)
+	data, _ = aesgcm.Open(nil, iv, data, nil)
+	return string(data)
+}
+
+func main() {
+	// passphrase, text
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Printf("Enter passphrase: ")
+	passphrase, _ := reader.ReadString('\n')
+
+	fmt.Printf("Enter text: ")
+	text, _ := reader.ReadString('\n')
+
+	c := encrypt(passphrase, text)
+	fmt.Println(c)
+	f, _ := os.OpenFile("example.txt", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	f.Write([]byte(c))
+	f.Write([]byte("\n"))
+	defer f.Close()
+	fmt.Println(decrypt(passphrase, text))
+}
