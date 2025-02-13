3c3ef21b85f5db53b16628605bae84838b43827e
Kevin <kevin@homelabwithkevin.com>
2025-02-13 18:43:24 -0500

    add webhook

---
+++ rocketchat/webhooks/outgoing.js
@@ -0,0 +1,88 @@
+/* exported Script */
+/* globals console, _, s, HTTP */
+
+/** Global Helpers
+ *
+ * console - A standard console instance
+ * _       - An underscore instance
+ * s       - An underscore string instance
+ * HTTP    - The Meteor HTTP object to do sync http calls
+ */
+
+class Script {
+  /**
+   * @params {object} request
+   */
+  prepare_outgoing_request({ request }) {
+    // request.params            {object}
+    // request.method            {string}
+    // request.url               {string}
+    // request.auth              {string}
+    // request.headers           {object}
+    // request.data.token        {string}
+    // request.data.channel_id   {string}
+    // request.data.channel_name {string}
+    // request.data.timestamp    {date}
+    // request.data.user_id      {string}
+    // request.data.user_name    {string}
+    // request.data.text         {string}
+    // request.data.trigger_word {string}
+    console.log(request)
+    let match
+
+    match = request.data.user_name.match(/rocket\.cat/);
+    if (match) { 
+      return null
+    } 
+
+    return {
+        url: request.url,
+        headers: request.headers,
+        method: 'POST',
+        data: request.data
+    }
+  }
+
+  /**
+   * @params {object} request, response
+   */
+  process_outgoing_response({ request, response }) {
+    // request              {object} - the object returned by prepare_outgoing_request
+
+    // response.error       {object}
+    // response.status_code {integer}
+    // response.content     {object}
+    // response.content_raw {string/object}
+    // response.headers     {object}
+
+    // Return false will abort the response
+    // return false;
+
+    // Return empty will proceed with the default response process
+    // return;
+    console.log(request)
+
+    return {
+      content: {
+        text: response.content,
+        parseUrls: false
+        // "attachments": [{
+        //   "color": "#FF0000",
+        //   "author_name": "Rocket.Cat",
+        //   "author_link": "https://open.rocket.chat/direct/rocket.cat",
+        //   "author_icon": "https://open.rocket.chat/avatar/rocket.cat.jpg",
+        //   "title": "Rocket.Chat",
+        //   "title_link": "https://rocket.chat",
+        //   "text": "Rocket.Chat, the best open source chat",
+        //   "fields": [{
+        //     "title": "Priority",
+        //     "value": "High",
+        //     "short": false
+        //   }],
+        //   "image_url": "https://rocket.chat/images/mockup.png",
+        //   "thumb_url": "https://rocket.chat/images/mockup.png"
+        // }]
+      }
+    };
+  }
+}
