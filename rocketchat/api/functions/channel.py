f7663731584f2a7ebb61d8dce956a7d983aa8326
Kevin <kevin@homelabwithkevin.com>
2025-02-18 21:24:43 -0500

    add list and delete channel

--- rocketchat/api/functions/channel.py
+++ rocketchat/api/functions/channel.py
@@ -1,8 +1,26 @@
-def delete_room(rocket, room_id):
-    print(f'Deleting room: {room_id}')
-    rocket.channels_delete(room_id)
+def delete(rocket, channel_id):
+    print(f'Deleting room: {channel_id}')
+    rocket.channels_delete(channel_id)
 
 def create(rocket, name, members):
   channel = rocket.channels_create(name, members=members).json()
   rocket.chat_post_message('Welcome!', channel=name)
   return channel
+
+def list(rocket, filter=None):
+    list_channels = []
+    channels = rocket.channels_list().json()
+
+    if filter:
+        print(f'Filtering: {filter}')
+
+    for channel in channels['channels']:
+        _id = channel['_id']
+        name = channel['name']
+        if filter:
+            if filter in name:
+                list_channels.append(_id)
+        else:
+            list_channels.append(_id)
+
+    return list_channels
--- rocketchat/api/main.py
+++ rocketchat/api/main.py
@@ -19,3 +19,7 @@ channel_name = None # Set this if you want to create a channel
 if channel_name:
   channel.create(rocket, channel_name, ['hxrsmurf'])
   integrations.create_outgoing(rocket, api, channel_name, outgoing_webhook_file)
+
+list_channels = channel.list(rocket=rocket, filter='testing')
+for _c in list_channels:
+  print(_c)
