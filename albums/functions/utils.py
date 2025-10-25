8722edb46dc1a5a6adc403cd805056a7eeb06200
Kevin <kevin@homelabwithkevin.com>
2025-10-25 08:44:46 -0400

    move create thumbnail to utils

--- albums/functions/utils.py
+++ albums/functions/utils.py
@@ -3,3 +3,12 @@ def random_string():
     import string
     return ''.join(random.choices(string.ascii_letters + string.digits, k=10))
 
+def create_thumbnail(source_file, output_file):
+    # https://stackoverflow.com/a/451580
+    base_width = 2048
+
+    with Image.open(source_file) as img:
+        width_percentage = (base_width / float(img.size[0]))
+        height_size = int((float(img.size[1]) * float(width_percentage)))
+        img = img.resize((base_width, height_size), Image.Resampling.LANCZOS)
+        img.save(output_file)
--- albums/main.py
+++ albums/main.py
@@ -76,15 +76,7 @@ def upload(bucket, source_file, target):
 
     print(f'Complete upload!')
 
-def create_thumbnail(source_file, output_file):
-    # https://stackoverflow.com/a/451580
-    base_width = 2048
 
-    with Image.open(source_file) as img:
-        width_percentage = (base_width / float(img.size[0]))
-        height_size = int((float(img.size[1]) * float(width_percentage)))
-        img = img.resize((base_width, height_size), Image.Resampling.LANCZOS)
-        img.save(output_file)
 
 
 def function_create_thumbnail():
