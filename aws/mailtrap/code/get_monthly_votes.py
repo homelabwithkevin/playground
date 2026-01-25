6b0eb2ec97bf54b7921454a6e14b1c4e6d22e197
Kevin <kevin@homelabwithkevin.com>
2026-01-25 08:23:49 -0500

    create get archive function

--- aws/mailtrap/code/get_monthly_votes.py
+++ aws/mailtrap/code/get_monthly_votes.py
@@ -2,7 +2,7 @@ import os
 import pandas as pd
 
 from dotenv import load_dotenv
-from functions import db
+from functions import db, utils
 
 load_dotenv()
 
@@ -14,38 +14,62 @@ def get_votes(newsletter='2024-12-28'):
     vote_results = db.get_votes(table_vote, newsletter)
     return vote_results
 
-archived_items = db.get_archive_items(table_archive)
+def test():
+    totals = []
 
-totals = []
+    # Iterate through Archived Items
+    # For Each Archived Item, Get Votes for all pictures
+    # Return Pandas Dataframe with all the data
 
-# Iterate through Archived Items
-# For Each Archived Item, Get Votes for all pictures
-# Return Pandas Dataframe with all the data
+    max = 3
+    for archived_item in archived_items:
+        newsletter = archived_item.split('-newsletter')[0]
+        votes = get_votes(newsletter)
+        _totals = {}
 
-max = 3
-for archived_item in archived_items:
-    newsletter = archived_item.split('-newsletter')[0]
-    votes = get_votes(newsletter)
-    _totals = {}
+        x = 0
+        for key, file in enumerate(votes):
+            x += 1
+            if x > max:
+                break
+            file_path = f'{cdn_url}/{newsletter}-newsletter/{file}'
 
-    x = 0
-    for key, file in enumerate(votes):
-        x += 1
-        if x > max:
-            break
-        file_path = f'{cdn_url}/{newsletter}-newsletter/{file}'
+            _totals = {
+                'newsletter': newsletter,
+                'rank': key,
+                'file': file,
+                'file_path': file_path,
+                'votes': votes[file]
+            }
 
-        _totals = {
-            'newsletter': newsletter,
-            'rank': key,
-            'file': file,
-            'file_path': file_path,
-            'votes': votes[file]
-        }
+            totals.append(_totals)
 
-        totals.append(_totals)
+    df = pd.DataFrame(totals)
+    print(df)
+    df.to_csv('totals.csv', index=False)
+    print(f'Saved to: totals.csv')
 
-df = pd.DataFrame(totals)
-print(df)
-df.to_csv('totals.csv', index=False)
-print(f'Saved to: totals.csv')
+def get_archive_items(table, save_to_file=True):
+    """
+        Helps paginate the dynamodb table for archive items (all newsletters)
+    """
+
+    archived_items = db.scan_paginate(table)
+    all_items = []
+    for items in archived_items:
+        for item in items:
+            # {'id': {'S': '2025-07-19-newsletter'}, 'order': {'S': '44'}}
+            all_items.append({
+                "order": item['order']['S'],
+                "id": item['id']['S']
+            })
+
+    df = pd.DataFrame(all_items)
+    df['order'] = pd.to_numeric(df['order'])
+    df = df.sort_values(by='order')
+    print(df)
+    if save_to_file:
+        file_name = utils.save_dataframe(dataframe=df, filename='archived-items')
+        return file_name
+
+archive_file_name = get_archive_items(table_archive, save_to_file=True)
