f3a431bd9b7d769490c25b0c1870ebc7c76db4c9
Kevin <kevin@homelabwithkevin.com>
2026-02-08 18:39:32 -0500

    create python test

--- aws/mailtrap/tests/tests.ps1
+++
@@ -1,5 +0,0 @@
-# AWS SAM Tests
-- https://docs.aws.amazon.com/serverless-application-model/latest/developerguide/using-sam-cli-local-generate-event.html
-
-# Generate
-sam local generate-event apigateway http-api-proxy > .\tests\http-api-proxy.json
