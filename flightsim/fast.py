c74f2e457433f40ce88beda66d68847d09318b37
Kevin <kevin@homelabwithkevin.com>
2026-02-17 19:22:59 -0500

    add my fast api implementation

---
+++ flightsim/fast.py
@@ -0,0 +1,105 @@
+from fastapi import FastAPI
+from haversine import haversine, Unit
+import pandas as pd
+import random
+
+app = FastAPI()
+
+
+def load_file():
+    # https://ourairports.com/airports.html
+    df = pd.read_csv('us-airports.csv')
+
+    selected_columns = [
+        'ident',
+        'type',
+        'name',
+        'latitude_deg',
+        'longitude_deg',
+        'elevation_ft',
+        'iso_country',
+        'iso_region',
+        'icao_code',
+        'iata_code',
+        'gps_code',
+        'local_code',
+        'scheduled_service',
+    ]
+
+    filtered_columns_df = df[selected_columns]
+    us_airports = filtered_columns_df[(filtered_columns_df['iso_country'] == 'US') & (filtered_columns_df['type'] != 'heliport') & (
+        filtered_columns_df['type'] != 'seaplane_base') & (filtered_columns_df['type'] != 'closed')]
+
+    us_airports = us_airports.fillna(0)
+    return us_airports
+
+
+def get_airport(icao: str):
+    data = load_file()
+    try:
+        df = data[data['ident'] == icao.upper()]
+        return df.to_dict('records')[0]
+    except Exception as e:
+        print(f'Failed to get airport: {icao}. Error: {e}')
+        return None
+
+
+def calculate_distance(source_airport, latitude_deg, longitude_deg):
+    source_coordinates = source_airport['latitude_deg'], source_airport['longitude_deg']
+    destination_coordinates = latitude_deg, longitude_deg
+    result = haversine(source_coordinates,
+                       destination_coordinates, unit=Unit.MILES)
+    return round(result, 4)
+
+
+def calculate_range(source: str, area: int):
+    data = load_file()
+    results = []
+
+    source_airport = get_airport(source)
+    data['distance_to'] = data.apply(lambda row: calculate_distance(
+        source_airport, row['latitude_deg'], row['longitude_deg']), axis=1)
+    data.drop(1, inplace=True)
+
+    # https://www.geeksforgeeks.org/pandas/drop-rows-from-the-dataframe-based-on-certain-condition-applied-on-a-column/
+    indices_to_drop = data[data['ident'] == source.upper()].index
+    data = data.drop(indices_to_drop)
+    data = data[data['distance_to'] <= area]
+    return data
+
+
+def choose_random_airport(data, sort):
+    df = data.sample(n=10)
+    if sort:
+        return df.sort_values('distance_to')
+    else:
+        return df
+
+
+# FAST API
+@app.get('/')
+async def root():
+    return {'message': 'Hello World'}
+
+
+@app.get('/airport/{icao}')
+async def airport_icao(icao: str):
+    return get_airport(icao)
+
+
+@app.get('/calculate/distance/{source}/{destination}')
+async def calculate_source_destination(source: str, destination: str):
+    result = calculate_distance(source=source, destination=destination)
+    return {'source': source.upper(), 'destination': destination.upper(), 'result': result}
+
+
+@app.get('/calculate/nearest/{source}/{area}')
+async def calcuate_nearest_airport(source: str, area: int):
+    results_range = calculate_range(source, area).sort_values('distance_to')
+    formatted_results = results_range.to_dict(orient='records')
+    random_results = choose_random_airport(
+        results_range, True).to_dict(orient='records')
+    print(pd.DataFrame(formatted_results))
+    print("------------------Random-------------------")
+    print(pd.DataFrame(random_results))
+    return {'source': source.upper(), 'random': random_results, 'all': formatted_results}
