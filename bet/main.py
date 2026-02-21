59fe84f180694e331bc3f738a31410e593f15132
Kevin <kevin@homelabwithkevin.com>
2026-02-21 14:46:39 -0500

    add events

--- bet/main.py
+++ bet/main.py
@@ -4,8 +4,10 @@ from dotenv import load_dotenv
 from fastapi import FastAPI
 from pydantic_settings import BaseSettings
 from fastapi.responses import HTMLResponse
+import csv
 
 from templates import pages
+from event import event
 
 load_dotenv()
 
@@ -20,6 +22,18 @@ app = FastAPI()
 
 @app.get("/items/", response_class=HTMLResponse)
 async def read_items():
+    all_events = []
+    with open('events.csv', newline='') as f:
+        reader = csv.reader(f)
+        for row in reader:
+            all_events.append(
+                {
+                    'title': row[0],
+                    'over': row[1],
+                    'under': row[2],
+                }
+            )
+
     return f"""
     <html>
         <head>
@@ -29,7 +43,12 @@ async def read_items():
         </head>
         <div class="flex justify-center pt-4">
             <div>
-                {pages.header(app_name=settings.app_name)}
+                <div>
+                    {pages.header(app_name=settings.app_name)}
+                </div>
+                <div>
+                    {event.events(all_events)}
+                </div>
             </div>
         </div>
     </html>
