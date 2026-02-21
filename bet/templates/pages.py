ab5dc748bb8af61c609a4001d872f9f3d18bffa8
Kevin <kevin@homelabwithkevin.com>
2026-02-21 14:18:56 -0500

    simple header

--- bet/templates/pages.py
+++ bet/templates/pages.py
@@ -1,6 +1,26 @@
 def header(app_name):
+    categories = [
+        "New",
+        "Sayings",
+        "Mentions",
+        "Tech",
+    ]
+
+    category_html = ""
+
+    for category in categories:
+        html = f"""
+        <div>
+            {category}
+        </div>
+        """
+        category_html += html
+
     return f"""
     <div>
         {app_name}
+    </div>
+    <div class="grid grid-cols-4 gap-4 pt-4">
+        {category_html}
     </div>
     """
