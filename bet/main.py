579f57eb3c3d6754e5a2d443fd3344793a928baf
Kevin <kevin@homelabwithkevin.com>
2026-02-21 13:55:05 -0500

    initial main.py

---
+++ bet/main.py
@@ -0,0 +1,20 @@
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+
+app = FastAPI()
+
+
+@app.get("/items/", response_class=HTMLResponse)
+async def read_items():
+    return """
+    <html>
+        <head>
+            <title>BET</title>
+            <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+            <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
+        </head>
+        <body>
+            <h1>Look ma! HTML!</h1>
+        </body>
+    </html>
+    """
