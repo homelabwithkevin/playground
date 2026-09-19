f2687d96ee8501df47b27fc6267098e0767d134c
Kevin <116416787+homelabwithkevin@users.noreply.github.com>
2026-09-19 06:57:23 -0400

    mailtrap - Updated Layer mapping, globals, and FunctionMemory (#136)
    * re-enable confirm_changeset for prod
    
    * move global to top-level for samconfig
    
    * add FunctionMemorySize parameter
    
    * exclude python folder from git
    
    * add headers to deploy.ps1
    
    * add layer helper script
    
    * create mappings for python layer
    
    ---------
    
    Co-authored-by: Kevin Murphy <kevin@hxrsmurf.info>

--- aws/mailtrap/deploy.ps1
+++ aws/mailtrap/deploy.ps1
@@ -1,11 +1,15 @@
 # Prod
+## Build
 sam build -t .\template.yaml
 sam deploy --config-env prod --config-file .\samconfig.yaml --template .\template.yaml
 
+## Sync / Live Edit
 sam sync -t .\template.yaml --watch --code --stack-name hlb-mailtrap-s3-prod
 
 # Develop
+## Build
 sam build -t .\template-develop.yaml
 sam deploy --config-env develop --config-file .\samconfig.yaml --template .\template-develop.yaml
 
+## Sync / Live Edit
 sam sync -t .\template-develop.yaml --watch --code --stack-name hlb-mailtrap-s3-develop
